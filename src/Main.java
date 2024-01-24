227223ad7111233aeaa5c62078f0d786e856a4c2
Stanislav <s.tyulkov@gmail.com>
2024-01-24 14:05:31 +0300

    Проверьте ДЗ №3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // 1 задание
+        // 1 Задача
         byte one = 1;
         System.out.println("Значение переменной one с типом byte равно " + one );
         short two = 20000;
@@ -14,7 +14,7 @@ public class Main {
         double six= 6.12345678;
         System.out.println("Значение переменной six с типом double равно " + six );
 
-        // 2 задание
+        // 2 Задача
         float f = 27.12F;
         System.out.println(f);
         long l = 987678965549L;
@@ -30,7 +30,7 @@ public class Main {
         byte b = 67;
         System.out.println(b);
 
-        // 3 задание
+        // 3 Задача
         byte lP = 23;
         byte aS = 27;
         byte eA = 30;
@@ -38,7 +38,7 @@ public class Main {
         int studentPaper = totaPaper / (lP + aS + eA);
         System.out.println("На каждого ученика рассчитано " + studentPaper + " листов бумаги");
 
-        // 4 задание
+        // 4 Задача
         byte bottleTwoMinute = 16;
         byte minute = 2;
         int bottleTwentyMinute = (bottleTwoMinute / minute) * 20;
@@ -51,7 +51,7 @@ public class Main {
         int bottleOneMonth = bottleOneDay * 30;
         System.out.println("За 30 дней машина произвела " + bottleOneMonth + " штук бутылок");
 
-        // 5 задание
+        // 5 Задача
         byte totalPaintJar = 120;
         byte whitePaintJarOneClass = 2;
         byte brownPaintJarOneClass = 4;
@@ -74,10 +74,40 @@ public class Main {
         int totalWeightIceCream = iceCream * weightOneIceCream;
         int totalWeightEggs = eggs * weightOneEgg;
         int totalWeightProduct = totalWeightBananas + totalWeightMilk + totalWeightIceCream + totalWeightEggs;
-        System.out.println("Вес завтрака составляет " + totalWeightProduct + " грамм");
+        System.out.println("Вес завтрака составляет " + totalWeightProduct + " гр");
         float totalWeightProductKilo = totalWeightProduct / 1000F;
-        System.out.println("Вес завтрака составляет " + totalWeightProductKilo + " килограмм");
+        System.out.println("Вес завтрака составляет " + totalWeightProductKilo + " кг");
 
+        // 7 Задача
+        byte weightLossKg = 7;
+        int weightLossGr = weightLossKg * 1000;
+        short weightOne = 250;
+        short weightTwo = 500;
+        int dayWeightLossOne = weightLossGr / weightOne;
+        System.out.println(dayWeightLossOne + " дней уйдет на похудение, если спортсмен будет терять каждый день по 250 гр");
+        int dayWeightLossTwo = weightLossGr / weightTwo;
+        System.out.println(dayWeightLossTwo + " дней уйдет на похудение, если спортсмен будет терять каждый день по 500 гр");
+        int dayWeightLossAverage = (dayWeightLossOne + dayWeightLossTwo) / 2;
+        System.out.println(dayWeightLossAverage + " дней в среднем уйдет на похудение, если спортсмен будет терять каждый день от 250 до 500 гр");
 
+        // 8 Задача
+        int maria = 67760;
+        int mariaYear = maria * 12;
+        int denis = 83690;
+        int denisYear = denis * 12;
+        int christina = 76230;
+        int christinaYear = christina * 12;
+        int mariaIncrease = maria * 110 / 100;
+        int mariaYearIncrease = mariaIncrease * 12;
+        int mariaYIDifference = mariaYearIncrease - mariaYear;
+        System.out.println("Маша теперь получает " + mariaIncrease + " руб. Годовой доход вырос на " + mariaYIDifference + " руб.");
+        int denisIncrease = denis * 110 / 100;
+        int denisYearIncrease = denisIncrease * 12;
+        int denisYIDifference = denisYearIncrease - denisYear;
+        System.out.println("Денис теперь получает " + denisIncrease + " руб. Годовой доход вырос на " + denisYIDifference + " руб.");
+        int christinaIncrease = christina * 110 / 100;
+        int christinaYearIncrease = christinaIncrease * 12;
+        int christinaYIDifference = christinaYearIncrease - christinaYear;
+        System.out.println("Кристина теперь получает " + christinaIncrease + " руб. Годовой доход вырос на " + christinaYIDifference + " руб.");
     }
 }
